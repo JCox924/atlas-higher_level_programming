41bc367361a89169e7020fdc515097eb26f1e68d
JCox924 <Joshua.Cox@atlasschool.com>
2024-05-07 12:44:07 -0500

    array approach on fizzbuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
+	results = []
 	for num in range(1, 101):
 		if num % 3 == 0 and num % 5 == 0:
-			print("FizzBuzz", end=' ')
+			results.append("FizzBuzz")
 		elif num % 3 == 0:
-			print("Fizz", end=' ')
+			results.append("Fizz")
 		elif num % 5 == 0:
-			print("Buzz", end=' ')
+			results.append("Buzz")
 		else:
-			print(num, end=' ')
-	print()
+			results.append(str(num))
+	print(" ".join(results))
