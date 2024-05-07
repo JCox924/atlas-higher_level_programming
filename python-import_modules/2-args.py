db1db91202eb3df5922ff3001eb1088dbeda6bd6
JCox924 <Joshua.Cox@atlasschool.com>
2024-05-07 17:41:54 -0500

    mark time taking a break 202405071745 due:202405072359

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,12 +6,12 @@ def main():
     num_args = len(argv) - 1  # exclude the script name itself
 
     if num_args == 0:
-        print("Number of arguments: 0.")
+        print("arguments: 0.")
     else:
         if num_args == 1:
-            print("Number of argument: 1:")
+            print("argument: 1:")
         else:
-            print("Number of arguments: {}:".format(num_args))
+            print("arguments: {}:".format(num_args))
         
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
