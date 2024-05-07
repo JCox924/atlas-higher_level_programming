017a820cb0f019cb7f8a18a9a1e20fa7ce1d2b0e
JCox924 <Joshua.Cox@atlasschool.com>
2024-05-07 12:08:10 -0500

    remainder of tasks

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,2 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
 print("".join(f"{chr(c)}" for c int range(97,123)), end="")
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for code in range(97, 123):
+	if code != 113 and code != 101:
+		print("{}".format(chr(code)), end='')
