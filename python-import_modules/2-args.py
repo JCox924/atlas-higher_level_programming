1bb253a993a6ac6bcf92f25ff1de404fc5366c66
JCox924 <Joshua.Cox@atlasschool.com>
2024-05-07 17:45:00 -0500

    fixed temp typo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,15 +6,15 @@ def main():
     num_args = len(argv) - 1  # exclude the script name itself
 
     if num_args == 0:
-        print("arguments: 0.")
+        print("arguments: 0.", end='')
     else:
         if num_args == 1:
-            print("argument: 1:")
+            print("argument: 1:", end='')
         else:
-            print("arguments: {}:".format(num_args))
+            print("arguments: {}:".format(num_args), end='')
         
         for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+            print("{}: {}".format(i, argv[i]), end='')
 
 if __name__ == "__main__":
     main()
