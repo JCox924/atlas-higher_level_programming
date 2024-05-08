722bc08355109d78aefb93e5415dae2e8b2b7216
JCox924 <Joshua.Cox@atlasschool.com>
2024-05-07 23:39:51 -0500

    correction to the correction

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 import hidden_4
 
 def print_sorted_names():
-    names = dir(hidden_4
-    for name in sorted):
+    names = dir(hidden_4)
+    for name in sorted:
 		if name[:2] != "__":
 			print(name)
 
