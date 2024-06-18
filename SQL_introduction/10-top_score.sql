868c9cac6bb46ade2756cca43f9d916a4e6e20f4
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-18 16:49:58 -0500

    'no cheating' even though bob is using a sql injection to change his score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- List all records from second_table
+-- Display score and name, ordered by score in descending order
+SELECT score, name FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- List all records from second_table with score >= 10
+-- Display score and name, ordered by score in descending order
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
