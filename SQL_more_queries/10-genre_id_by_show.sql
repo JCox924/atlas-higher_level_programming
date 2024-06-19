d8bf6745f434086b88abcdf2df26f1d57b27e82e
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-18 21:36:31 -0500

    went back to original plan

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,11 +1,5 @@
--- Specifies database to use
-USE hbtn_0d_tvshows;
-
--- List all shows with at least one genre linked
--- Each record should display: tv_shows.title - tv_show_genres.genre_id
--- Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT show.'title', g.'genre_id'
-	FROM 'tv_shows' AS show
-		INNER JOIN 'tv_show_genres' AS genre
-		ON show.'id' = genre.'show_id'
-ORDER BY show.'title', genre.'genre_id';
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,11 +1,7 @@
--- Specifes database to use
-USE hbtn_0d_tvshows;
-
 -- List all shows with their genres from the database hbtn_0d_tvshows
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- Include shows without genres (display NULL for genre_id)
-
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
