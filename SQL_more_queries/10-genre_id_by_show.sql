0e5dc742e3a13e9b5d93555ab05832cea5a5f0c2
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-18 21:32:06 -0500

    test for 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,8 @@ USE hbtn_0d_tvshows;
 -- List all shows with at least one genre linked
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT show.'title', g.'genre_id'
+	FROM 'tv_shows' AS show
+		INNER JOIN 'tv_show_genres' AS genre
+		ON show.'id' = genre.'show_id'
+ORDER BY show.'title', genre.'genre_id';
