acfe142061e6b3b1aa32a572e33802bd35bb97e1
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-20 16:23:31 -0500

    same with model_state.py code needed to be inside of if __name__ = __main__ block to avoid function being executed

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+"""
+This module will define the class State and init Base.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-def find_state(username, password, database, state_name):
-    # Create the database engine
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
 
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
-    # Create a Session
-    session = Session()
 
-    # Query the database to get the State object with the given name
-    state = session.query(State).filter(State.name == state_name).first()
-
-    # Print the result
-    if state:
-        print(f"{state.id}")
-    else:
-        print("Not found")
-
-    # Close the session
-    session.close()
-
-if __name__ == "__main__":
-    # The script should only execute when run directly, not when imported
-    if len(sys.argv) == 5:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
-        find_state(username, password, database, state_name)
+class State(Base):
+    """State class with id and name fields."""
+    __tablename__ = 'states'
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+    name = Column(String(128), nullable=False)
