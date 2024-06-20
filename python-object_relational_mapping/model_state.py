388c907a669a30ab9bf87d2ca3023ac85ef6b006
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-20 16:20:17 -0500

    re-do for model_state, inspected code with pycharm code inspector, removed unnecessary function

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, create_engine
+"""
+This module will define the class State and init Base.
+"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+
 
 Base = declarative_base()
 
+
 class State(Base):
+    """State class with id and name fields."""
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
     name = Column(String(128), nullable=False)
-
-def main(username, password, database):
-    # Create the database engine
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
-
-    # Create all tables in the database
-    Base.metadata.create_all(engine)
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        main(username, password, database)
