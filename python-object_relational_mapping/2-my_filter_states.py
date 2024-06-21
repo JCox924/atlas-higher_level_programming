48aa6e7a88f40c329f8be5f8bdfe8ab9ec02f2aa
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-20 20:08:37 -0500

    broke the code, re-doing from previous iteration

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-"""
-This module lists all states with a name
-starting with N (upper N) from the database hbtn_0e_0_usa.
-"""
-import MySQLdb
 import sys
+import MySQLdb
 
+if __name__ == "__main__":
+    # Retrieve the arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
+    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
+
+    # Create a cursor object to interact with the database
     cursor = db.cursor()
-    cursor.execute(
-        """SELECT * FROM states WHERE name
-        LIKE BINARY 'N%' ORDER BY states.id"""
-    )
+
+    # Prepare and execute the SQL query
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows in a list of lists.
     rows = cursor.fetchall()
+
+    # Print the fetched rows
     for row in rows:
         print(row)
+
+    # Close the cursor and the connection
     cursor.close()
     db.close()
--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""
-This module lists all states with name
-starting with N from database hbtn_0e_0_usa.
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = db.cursor()
-    query = "SELECT * FROM  states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-    rows = cursor.fetchall()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
