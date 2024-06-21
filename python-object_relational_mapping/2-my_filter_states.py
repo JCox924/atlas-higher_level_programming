0605f1c5c1cd1dd7aa31996b1d4a29024c445fdb
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-20 20:17:24 -0500

    focusing on 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,40 +3,10 @@
 This script connects to a MySQL database and retrieves all values in the 'states'
 table where the name matches the argument provided
 """
-import sys
-import MySQLdb
-
-
 if __name__ == "__main__":
-    # Retrieve the arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
-
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Prepare and execute the SQL query
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch all the rows in a list of lists.
-    rows = cursor.fetchall()
-
-    # Print the fetched rows
-    for row in rows:
-        print(row)
-
-    # Close the cursor and the connection
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
