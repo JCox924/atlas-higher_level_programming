5703da33e10f67a5fc668d2ac85961164ce123c3
JCox924 <joshua.cox@atlasschool.com>
2024-07-23 19:41:59 -0500

    more semistandard fixes

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -7,7 +7,7 @@ function factorial (num) {
   if (isNaN(num) || num <= 1) {
     return 1;
   }
-  return num * factorial (num - 1);
+  return num * factorial(num - 1);
 }
 
 console.log(factorial(n));
