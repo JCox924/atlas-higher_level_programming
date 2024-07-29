4def91dc0e966cf917aeebe90cf57aa7908988e3
JCox924 <joshua.cox@atlasschool.com>
2024-07-28 19:35:57 -0500

    add 101-starwars_characters.js, similar to 100-starwars_characters.js but prints character in same order as /films/ response

---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characterUrls = filmData.characters;
+
+  function printCharacterNames(urls, index = 0) {
+    if (index >= urls.length) {
+      return;
+    }
+
+    request.get(urls[index], (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+        return;
+      }
+
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+      printCharacterNames(urls, index + 1);
+    });
+  }
+
+  printCharacterNames(characterUrls);
+});
