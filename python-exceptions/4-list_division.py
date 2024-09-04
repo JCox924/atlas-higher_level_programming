e280eb4f7c7c26a4ec045e2be8b48c847baa9563
JCox924 <joshua.cox@atlasschool.com>
2024-09-03 22:50:04 -0500

    4.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,22 +6,21 @@ def list_division(my_list_1, my_list_2, list_length):
             num1 = my_list_1[i]
             num2 = my_list_2[i]
 
-            division_result = num1 / num2
 
-            result.append(int(division_result) if division_result == 0 else division_result)
+            result.append(float(num1) / float(num2))
         except IndexError:
             print("out of range")
-            result.append(0)
+            result.append(0.0)
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
+            result.append(0.0)
         except TypeError:
             print("wrong type")
-            result.append(0)
+            result.append(0.0)
         finally:
             if len(result) > list_length:
                 result = result[:list_length]
             while len(result) < list_length:
-                result.append(0)
+                result.append(0.0)
 
     return result
