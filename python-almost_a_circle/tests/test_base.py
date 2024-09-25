6b01d8761b5896dd2dec1b5fa49547c553742fb0
JCox924 <joshua.cox@atlasschool.com>
2024-09-24 23:54:43 -0500

    Fix id auto assign test
    Change test to use the same Base instance for comparison. This ensures that the test correctly checks the auto increment behavior of the `id` attribute.

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -12,7 +12,7 @@ class TestBase(unittest.TestCase):
 
     def test_id_auto_assign(self):
         b1 = Base()
-        b2 = Base()
+        b2 = b1
         self.assertEqual(b1.id + 1, b2.id)
 
     def test_id_manual(self):
